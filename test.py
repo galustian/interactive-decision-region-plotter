280ec3fd68012f381fa3554b7f38e445dc7c80e6
galustian <leonard@galustian.com>
2018-05-04 20:11:25 +0200

    change test classifier

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.svm import SVC
 from plotter import plot_decision_region
 
-clf = GradientBoostingClassifier()
+clf = SVC()
 plot_decision_region(clf)
